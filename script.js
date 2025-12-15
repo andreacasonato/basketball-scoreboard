34904adb433e28c2a727f2e7619d5c8e258b2a07
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-12-15 16:00:36 +0100

    Add 1 point functionality

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const homePointsThree = document.getElementById("home-point-3");
 const homeFoulsDisplay = document.getElementById("home-fouls-current");
 const homeFoulsOne = document.getElementById("home-fouls-1");
 const homeFoulsReset = document.getElementById("home-fouls-reset");
+
+/* ========================== */
+
+homePointsOne.addEventListener("click", () => {
+  const currentText = homePointsDisplay.innerText;
+
+  const currentNumber = Number(currentText);
+
+  const newNumber = currentNumber + 1;
+
+  homePointsDisplay.innerText = newNumber;
+});
