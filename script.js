66db19179b1eb6667ca51ebb56fb6df117cdee3d
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-12-15 12:36:00 +0100

    Get home HTML elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const homePointsDisplay = document.getElementById("home-points");
+const homePointsOne = document.getElementById("home-point-1");
+const homePointsTwo = document.getElementById("home-point-2");
+const homePointsThree = document.getElementById("home-point-3");
+
+const homeFoulsDisplay = document.getElementById("home-fouls-current");
+const homeFoulsOne = document.getElementById("home-fouls-1");
+const homeFoulsReset = document.getElementById("home-fouls-reset");
